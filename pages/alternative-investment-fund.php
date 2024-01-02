323cecc320c0c962f81bc4e38aa4d820018acfd7
YasinShaikh123 <123150391+YasinShaikh123@users.noreply.github.com>
2024-01-02 17:42:56 +0530

    yasin commit

---
+++ pages/alternative-investment-fund.php
@@ -0,0 +1,207 @@
+<?php $currentPage = "alternative-investment-fund" ?>
+<?php include '../component/header_link.php'; ?>
+<?php include '../component/header.php'; ?>
+
+<div>
+    <div class="inner-banner-funds">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Alternative Investment Funds</h2>
+                    <h4>Enhance your Portfolio's Risk-Adjusted Returns by<br> investing througt AIFs.</h4>
+                    <p>Alternative Investment Funds or AIFs are privately<br> pooled investment vehicles that collect funds from<br> investors, under specific guidelines of the SEBI. It<br> includes Venture Debt, hedge funds, real estate<br> funds, PIPE, Private Equity,etc.</p>
+                    <div class="banner-btn mt-4">
+                        <a href="#">View Investments</a>
+                    </div>
+                </div>
+                <div class="col-md-6  d-flex align-center">
+                     <div class="banner-profile">
+                        <h5>Ideal Investment Profile:</h5>
+                        <div class="profile d-flex">
+                            <img src="../assets/image/profile-banner.svg">
+                            <div>
+                                <p>Expected Return (IRR): ~12-24% p.a.</p>
+                                <p>Suggested Horizon: More than 5 Years</p>
+                                <p>Minimum Investment: ₹ 1,00,00,000</p>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="your-investment">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-ms-12">
+                <h2 class="invest-fund">KYI: Know Your Investment</h2>
+                <div>
+                    <h3>What are AIFs?</h3>
+                    <p>Alternative Investment Funds (AIFs) in India are investment vehicles that collect money from sophisticated private investors, both Indian and foreign, to invest it according to a stated investment strategy. </p>
+                </div>
+                <div>
+                    <h3>Why Invest in AIFs?</h3>
+                    <p>Unlike mutual funds or exchange-traded funds (ETFs), which typically invest in public equities or fixed-income securities, AIFs often focus on alternatives like venture capital, private equity, hedge funds, and managed futures. These funds are seen as important for the growth of the Indian economy as they can provide the necessary capital for startups and other sectors that do not have easy access to traditional forms of finance.</p>
+                </div>
+                <div>
+                    <h3>Who can Invest in AIF?</h3>
+                    <p>Any investor whether Indian, foreign, or non-resident Indian can invest in an AIF, as long as she/he has the required funds for investment, and is willing to take a risk on the unlisted and illiquid securities. To invest in an AIF investor must provide proof of income, PAN card, and ID proof.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-ms-12">
+                <div class="structure">
+                    <img src="../assets/image/structure.png">
+                </div>
+                <h3>Key Features of AIFs</h3>
+                <ul class="key-feature">
+                    <li>SEBI Registered</li>
+                    <li>Private Capital</li>
+                    <li>Non-Traditional Assets</li>
+                    <li>Limited Liquidity</li>
+                    <li>For Wealthy Investors</li>
+                    <li>High Risk and Reward</li>
+                </ul>
+                <div>
+                    <h3>What is the minimum amount needed to invest in AIF?</h3>
+                    <p>All AIF categories in India except "angel fund" need a minimum investment of Rs. 1 crore, whereas, for the angel fund, that amount is Rs. 25 lakhs. In the case of an employee or director of AIF, the minimum value of investment shall be Rs. 25 lakhs.</p>
+                </div>
+            </div>
+        </div>
+            <div class="text-center mb-3 mt-2">
+                <h3>Popularity of Alternative Investment Funds</h3>
+            </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <ul>
+                    <li>Alternate Investment Markets are the newest investment trend, especially for higher net-worth individuals. With the digitization of retail investing practices and the newest trend of fractionalization, Alternative Investments are gaining popularity in India, as it gives Indian investors more opportunities to reach their investment goals faster because of the higher rates of returns, and diversify their portfolio. </li>
+                    <li>The alternative industry is expected to reach ₹43.64 lakh crore by 2028, potentially surpassing USD 300 billion.</li>
+                    <li>From June FY19 to June FY24, the industry saw a 36% CAGR, reaching AUM of ₹8.45 lakh crore in June FY24. This growth rate exceeds the mutual fund industry's 13% CAGR, demonstrating the robust expansion of alternative investments.</li>
+                    <li>As of November 2023, over 1200 Alternative Investment Funds (AIFs) registered with SEBI.</li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <p>The following factors have contributed to the growth of AIF markets in India:</p>
+                <ul>
+                    <li>Rising <strong> investor knowledge </strong> and interest in alternative investments.</li>
+                    <li>Strong <strong> economic expansion </strong> in India.</li>
+                    <li>A supportive <strong> regulatory </strong> framework.</li>
+                    <li><strong> Increased income </strong> among Indian residents and greater wealth leading HNIs to seek out alternative investments like PMSs and AIFs for their potential rewards and specialized offerings.</li>
+                    <li>Widespread <strong> availability of information. </strong></li>
+                    <li>The appeal of <strong> portfolio diversification </strong> and the potential for <strong> higher returns.</strong></li>
+                    <li>Initiatives like <strong> Gift City </strong> creating a <strong> favorable financial ecosystem </strong> for AIFs and attracting foreign investments.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="historical">
+            <h2 class="inner-heading text-center">Historical Growth and Category-wise Distribution of AIFs</h2>
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <img src="../assets/image/assets-two.png">
+                </div>     
+                <div class="col-md-6 col-sm-12">
+                    <img src="../assets/image/circle-graph.svg">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-6 col-sm-12">
+                <div class="hist-card">
+                    <h2 class="inner-heading mb-3">AIF vs Mutual Funds</h2>
+                    <p>Alternative Investment Funds offer a higher degree of flexibility than mutual funds as they invest in unlisted shares and also use shorting and leverage</p>
+                    <p>For HNIs who are looking to diversify their portfolios and increase long-term returns on their investments while willing to take a considerable risk, AIFs seem like decent investment alternatives. But for those who can take low to high risk, mutual funds are the way to go. The decision between alternative investment funds and mutual funds should be based on your investment objectives, available capital, and long-term intentions.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="hist-card">
+                    <h2 class="inner-heading mb-3">AIF vs PMS</h2>
+                    <p>In Portfolio Management System (PMS), each investor portfolio is different, and it’s a customized offering. PMS cannot pool money from different investors to create an investment fund. PMS investors can also choose to exit their investments anytime they want. On the other hand, AIFs pool money from investors and have a lock-in period ranging from 3 to 5 years.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="banifit-tabs mt-4">
+        <div class="container">
+            <nav>
+                <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
+                    <button class="nav-link benefits active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Benefits</button>
+                    <button class="nav-link risks" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Risks</button>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane benefits fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <ul>
+                        <li><strong> Regulations are in place:</strong> AIF is regulated under SEBI AIF Regulations. As per SEBI, if investors pool funds to invest collectively, they can only do it through AIF. They cannot invest through LLP, Private Limited Companies, etc.</li>
+                        <li><strong>Higher Returns: </strong> AIFs give investors exposure to alternate securities and provide higher returns as compared to traditional investments. Therefore, by investing in AIFs, investors improve the overall performance of their portfolios and help achieve long-term capital appreciation. </li>
+                        <li><strong>Low Volatility:</strong> AIF investments, unlike traditional investments, are not dependent on the ups and downs of the market and hence less volatile as compared to traditional equity investments. </li>
+                        <li><strong>Diversification: </strong> AIF gives vast scope for diversification due to its flexibility. Fund managers have the scope to adopt multiple investment options and market strategies. This, in turn, helps in the diversification of a portfolio.</li>
+                        <li><strong>Better Negotiation with AIF: </strong> AIF deals with pooled funds that are invested in good-sized amounts, and hence provide better negotiating powers to the investors. In traditional investments, investors only get minimal ownership. </li>
+                        <li><strong>Professional Guidance: </strong> Money is managed by expert professionals</li>
+                    </ul>
+                </div>
+                <div class="tab-pane risks fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <ul>
+                        <li>Although AIF is not correlated with conventional investments, these funds are also subject to <strong> higher volatility. </strong> 
+                        Sometimes it does happen that if an AIF idea does not work in the market, the funds suffer heavy losses.</li>
+                        <li><strong>The liquidity crisis</strong> is also another downside of AIF investments. Even after the lock-in is over, sometimes you cannot withdraw your investment completely. </li>
+                        <li>Significant credit defaults on debt papers are also a possibility. </li>
+                        <li>The<strong> expense ratios</strong>  are also very high in AIFs. <strong>High taxation rates, </strong> especially on the Category III AIFs may reduce the returns you receive from the fund.</li>
+                    </ul>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <div class="categories mt-5">
+        <div class="container">
+            <h2>Categories of AIF</h2>
+            <p class="mb-2">AIFs are classified into three categories based on their investment strategies and risk profiles:</p>
+            <ul class="mb-5">
+                <li><strong>Category I AIFs </strong> - AIF that invest in early-stage start-ups, SMEs, and social ventures that the government or regulators consider socially or economically desirable.</li>
+                <li><strong>Category II AIFs </strong>  - These funds invest in private equity, debt or other securities of companies, asset securitization or any other structure as permitted under SEBI regulations. Category II AIF are AIFs which does not fall in Category I and III and which do not undertake leverage or borrowing other than to meet day-to-day operational requirements.</li>
+                <li><strong>Category III AIFs </strong> - These funds use complex trading strategies such as leverage, short-selling, and derivatives to generate high returns.</li>
+            </ul>
+            <div class="categories-card">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="cate-card">
+                            <h4>AIF Category I</h4>
+                            <div class="mb-3">
+                                <img src="../assets/image/cate-one.svg">
+                            </div>
+                            <div class="btn">
+                                <a href="#">View Category Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="cate-card">
+                            <h4>AIF Category II</h4>
+                            <div class="mb-3">
+                                <img src="../assets/image/cate-two.png">
+                            </div>
+                            <div class="btn">
+                                <a href="#">View Category Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="cate-card">
+                            <h4>AIF Category III</h4>
+                            <div class="mb-3">
+                                <img src="../assets/image/cate-three.png">
+                            </div>
+                            <div class="btn">
+                                <a href="#">View Category Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include '../component/footer.php'; ?>
+<?php include '../component/footer_link.php'; ?>
--- pages/privacy-policy.php
+++ pages/privacy-policy.php
@@ -10,7 +10,7 @@
 
 <div class="privacy-content">
     <div class="container">
-        <h1><strong>Privacy Policy</strong></h1>
+        <h1>Privacy Policy</h1>
         <p>
             Blue Feather Ventures Private Limited is committed to protecting the privacy and security of our users'
             personal
@@ -28,7 +28,7 @@
             safeguard your right to privacy, and only use the information you provide in ways that are beneficial to
             you.
         </p>
-        <h2><strong>COLLECTION AND USE OF PERSONAL INFORMATION</strong></h2>
+        <h2>COLLECTION AND USE OF PERSONAL INFORMATION</h2>
         <p>
             We may collect personal information from you when you create an account or use the App, including your name,
             email
@@ -44,7 +44,7 @@
             best of our
             abilities, to serve your purpose.
         </p>
-        <h2><strong>DISCLOSURE OF PERSONAL INFORMATION</strong></h2>
+        <h2>DISCLOSURE OF PERSONAL INFORMATION</h2>
         <p>
             We may share your personal information with third-party service providers that help us operate and improve
             our services
@@ -57,7 +57,7 @@
             to impose
             strict confidentiality standards and practices on the third party to protect your privacy.
         </p>
-        <h2><strong>DATA RETENTION AND SECURITY</strong></h2>
+        <h2>DATA RETENTION AND SECURITY</h2>
         <p>
             We will retain your personal information for as long as necessary to provide the App and comply with legal
             and
@@ -80,7 +80,7 @@
             those websites.
             Read the privacy policy of other websites carefully. We are not responsible for these third-party sites.
         </p>
-        <h2><strong>YOUR CHOICES AND RIGHTS</strong></h2>
+        <h2>YOUR CHOICES AND RIGHTS</h2>
         <p>
             You may choose to opt out of certain types of communications from us, such as marketing emails. You may also
             have the
@@ -88,7 +88,7 @@
             using the
             contact information provided below.
         </p>
-        <h2><strong>CHANGES TO THIS PRIVACY POLICY</strong></h2>
+        <h2>CHANGES TO THIS PRIVACY POLICY</h2>
         <p>
             We also reserve the right to change our privacy policy any time with or without notifying users of our new
             privacy
@@ -98,54 +98,12 @@
             visiting and/or
             using our website, you agree with this privacy policy.
         </p>
-        <h2><strong>CONTACT US</strong></h2>
+        <h2>CONTACT US</h2>
         <p>
             If you have any questions about this Policy or other privacy concerns, you can also email us at
-            <a href="">info@freeu.in</a>.
+            <a mailto:href="">info@freeu.in</a>.
         </p>
-        <!-- <h2>1. What Information Does the Service Collect?</h2>
-        <p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use,
-            all applicable laws and regulations, and agree that you are responsible for compliance with any
-            applicable local laws.
-        </p>
-        <p>
-            If you do not agree with any of these terms, you are prohibited from using or accessing this site. The
-            materials contained in this web site are protected by applicable copyright and trade mark law.
-        </p>
-        <h2>2. Information You Provide About a Third Party</h2>
-        <p>
-            Whilst we try to ensure that the standard of the Website remains high and to maintain the continuity of it,
-            the internet is not an inherently stable medium, and errors, omissions, interruptions of service and delays
-            may occur at any time.
-        </p>
-        <p>
-
-            We do not accept any liability arising from any such errors, omissions, interruptions or delays or any
-            ongoing obligation or responsibility to operate the Website (or any particular part of it) or to provide the
-            service offered on the Website. We may vary the specification of this site from time to time without notice.
-        </p>
-        <h2>3. Revisions and Errata</h2>
-        <p>
-            You may only use the Website for lawful purposes when seeking employment or help with your career,
-            when purchasing training courses or when recruiting staff. You must not under any circumstances seek
-            to undermine the security of the Website or any information submitted to or available through it. In
-            particular, but without limitation, you must not seek to access, alter or delete any information to which
-            you do not have authorised access, seek to overload the system via spamming or flooding, take any
-            action or use any device, routine or software to crash, delay, damage or otherwise interfere with the
-            operation of the Website or attempt to decipher, disassemble or modify any of the software, coding or
-            information comprised in the Website.
-        </p>
-        <h2>4. How Do We Use the Information Collected?</h2>
-        <p>
-            Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly
-            vehement irresistibly fussy penguin insect additionally wow absolutely crud meretriciously hastily
-            dalmatian a glowered inset one echidna cassowary some parrot and much as goodness some froze the
-            sullen much connected bat wonderfully on instantaneously.
-        </p>
-        <p>
-            Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly
-            vehement irresistibly fussy penguin insect additionally.
-        </p> -->
+       
     </div>
 </div>
 
